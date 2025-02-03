3d499a36e0967e82d80a9a8328a8d3f7f94a66e1
Scott Fairclough <scott@hexosoft.co.uk>
2025-02-03 14:21:53 +0000

    more workers and jobs

---
+++ jobs/good-sender.go
@@ -0,0 +1,133 @@
+package jobs
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"errors"
+	"fmt"
+	"math/big"
+	"math/rand"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/hashicorp/go-hclog"
+)
+
+type GoodSender struct {
+	Config   GoodSenderConfig
+	quit     chan struct{}
+	done     chan struct{}
+	instance uint64
+}
+
+type GoodSenderConfig struct {
+	JobConfig
+}
+
+func NewGoodSender(instance uint64) (*GoodSender, error) {
+	return &GoodSender{
+		Config:   GoodSenderConfig{},
+		quit:     make(chan struct{}),
+		done:     make(chan struct{}),
+		instance: instance,
+	}, nil
+}
+
+func (g *GoodSender) SetWallet(address *common.Address, privateKey *ecdsa.PrivateKey, chainID *big.Int) {
+	g.Config.Address = address
+	g.Config.Key = privateKey
+	g.Config.ChainID = chainID
+}
+
+func (g *GoodSender) Run(client *ethclient.Client, log hclog.Logger) error {
+	log = log.With("job", g.Name(), "instance", g.instance)
+
+	totalSent := 0
+
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-g.quit:
+			log.Info("received signal, stopping")
+			close(g.done)
+			return nil
+		default:
+			nonce, err := client.PendingNonceAt(context.Background(), *g.Config.Address)
+			if err != nil {
+				return err
+			}
+
+			randomAddress := common.HexToAddress(fmt.Sprintf("0x%x", rand.Intn(1000000000000000000)))
+
+			tx := types.NewTransaction(nonce, randomAddress, big.NewInt(1000), 21000, big.NewInt(2000000000), nil)
+
+			tx, err = types.SignTx(tx, types.NewEIP155Signer(g.Config.ChainID), g.Config.Key)
+			if err != nil {
+				return err
+			}
+
+			err = client.SendTransaction(context.Background(), tx)
+			if err != nil {
+				log.Error("failed to send transaction", "error", err)
+				time.Sleep(1 * time.Second)
+				continue
+			}
+
+			// now get the receipt
+			receiptCount := 0
+			for {
+				select {
+				case <-g.quit:
+					log.Info("received signal, stopping")
+					return nil
+				default:
+				}
+
+				receipt, err := client.TransactionReceipt(context.Background(), tx.Hash())
+				if err != nil {
+					if errors.Is(err, ethereum.NotFound) {
+						time.Sleep(500 * time.Millisecond)
+						receiptCount++
+						if receiptCount > 50 {
+							log.Error("waited too long for receipt", "error", err)
+							time.Sleep(3 * time.Second)
+							continue
+						}
+						continue
+					} else {
+						log.Error("failed to get transaction receipt", "error", err)
+						time.Sleep(1 * time.Second)
+						continue
+					}
+				}
+				if receipt.Status == types.ReceiptStatusSuccessful {
+					totalSent++
+					break
+				} else {
+					log.Warn("transaction failed", "hash", tx.Hash().Hex())
+					break
+				}
+			}
+
+			select {
+			case <-ticker.C:
+				log.Info("sent transactions", "from", g.Config.Address.Hex(), "total", totalSent)
+			default:
+			}
+		}
+	}
+}
+
+func (g *GoodSender) Stop() <-chan struct{} {
+	close(g.quit)
+	return g.done
+}
+
+func (g *GoodSender) Name() string {
+	return "good-sender"
+}
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"crypto/ecdsa"
+	"errors"
 	"flag"
 	"math/big"
 	"os"
@@ -11,6 +12,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -23,7 +25,14 @@ import (
 var (
 	privateKey string
 	rpcURL     string
+
+	fundAmount = big.NewInt(1000000000000000000)
 )
+
+type Wallet struct {
+	Address    *common.Address
+	PrivateKey *ecdsa.PrivateKey
+}
 
 func main() {
 	flag.StringVar(&privateKey, "private-key", "", "main account private key used to fund accounts for the jobs")
@@ -51,12 +60,20 @@ func main() {
 	}
 	parentAddress := crypto.PubkeyToAddress(parentKey.PublicKey)
 
+	log.Info("parent address", "address", parentAddress.Hex())
+
 	client, err := ethclient.Dial(rpcURL)
 	if err != nil {
 		log.Error("failed to dial rpc", "error", err)
 		return
 	}
 	defer client.Close()
+
+	parentNonce, err := client.PendingNonceAt(context.Background(), parentAddress)
+	if err != nil {
+		log.Error("failed to get parent nonce", "error", err)
+		return
+	}
 
 	allJobs, err := createJobs()
 	if err != nil {
@@ -70,25 +87,35 @@ func main() {
 		return
 	}
 
-	for _, job := range allJobs {
-		address, privateKey, err := createWallet()
-		if err != nil {
-			log.Error("failed to create wallet", "error", err)
-			return
-		}
-		err = fundWallet(client, &parentAddress, parentKey, address, big.NewInt(1000000000000000000), chainID)
-		if err != nil {
-			log.Error("failed to fund wallet", "error", err)
-			return
-		}
-		job.SetWallet(address, privateKey, chainID)
-		go func(j jobs.Job, address *common.Address, privateKey *ecdsa.PrivateKey) {
-			log.Info("starting job", "name", j.Name(), "address", address.Hex())
+	fundingHashes, walletKeys, err := fundWallets(client, parentNonce, &parentAddress, parentKey, len(allJobs), fundAmount, chainID, log)
+	if err != nil {
+		log.Error("failed to fund wallets", "error", err)
+		return
+	}
+
+	allMined, err := waitUntilMined(client, fundingHashes)
+	if err != nil {
+		log.Error("failed to wait for funding transactions to be mined", "error", err)
+		return
+	}
+
+	if allMined {
+		log.Info("all funding transactions mined")
+	} else {
+		log.Error("failed to mine all funding transactions")
+		return
+	}
+
+	for i, job := range allJobs {
+		wallet := walletKeys[i]
+		job.SetWallet(wallet.Address, wallet.PrivateKey, chainID)
+		go func(j jobs.Job, wallet Wallet) {
+			log.Info("starting job", "name", j.Name(), "address", wallet.Address.Hex())
 			err = j.Run(client, log)
 			if err != nil {
 				log.Error("problem running job", "name", j.Name(), "error", err)
 			}
-		}(job, address, privateKey)
+		}(job, wallet)
 	}
 
 	// now wait for sigint or sigterm
@@ -122,6 +149,15 @@ func createJobs() ([]jobs.Job, error) {
 		result = append(result, alreadyExists)
 	}
 
+	// create 10 good sender jobs
+	for i := 0; i < 600; i++ {
+		goodSender, err := jobs.NewGoodSender(uint64(i))
+		if err != nil {
+			return result, err
+		}
+		result = append(result, goodSender)
+	}
+
 	monitor, err := jobs.NewMonitor()
 	if err != nil {
 		return result, err
@@ -145,28 +181,92 @@ func createWallet() (*common.Address, *ecdsa.PrivateKey, error) {
 
 func fundWallet(
 	client *ethclient.Client,
+	nonce uint64,
 	parentAddress *common.Address,
 	parentKey *ecdsa.PrivateKey,
 	destAddress *common.Address,
 	value *big.Int,
 	chainID *big.Int,
-) error {
-	nonce, err := client.PendingNonceAt(context.Background(), *parentAddress)
-	if err != nil {
-		return err
-	}
-
+) (common.Hash, error) {
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
-		return err
+		return common.Hash{}, err
 	}
 
 	tx := types.NewTransaction(nonce, *destAddress, value, 21000, gasPrice, []byte{})
 
 	tx, err = types.SignTx(tx, types.NewEIP155Signer(chainID), parentKey)
 	if err != nil {
-		return err
+		return common.Hash{}, err
 	}
 
-	return client.SendTransaction(context.Background(), tx)
+	err = client.SendTransaction(context.Background(), tx)
+	if err != nil {
+		return common.Hash{}, err
+	}
+
+	return tx.Hash(), nil
+}
+
+func fundWallets(
+	client *ethclient.Client,
+	nonce uint64,
+	parentAddress *common.Address,
+	parentKey *ecdsa.PrivateKey,
+	count int,
+	amount *big.Int,
+	chainID *big.Int,
+	log hclog.Logger,
+) ([]common.Hash, []Wallet, error) {
+	wallets := make([]Wallet, count)
+	hashes := make([]common.Hash, count)
+	for i := 0; i < count; i++ {
+		address, privateKey, err := createWallet()
+		if err != nil {
+			return nil, nil, err
+		}
+		wallets[i] = Wallet{
+			Address:    address,
+			PrivateKey: privateKey,
+		}
+		log.Info("created wallet", "address", address.Hex())
+		hash, err := fundWallet(client, nonce, parentAddress, parentKey, address, amount, chainID)
+		if err != nil {
+			return nil, nil, err
+		}
+		log.Info("funded wallet", "address", address.Hex(), "hash", hash.Hex())
+		hashes[i] = hash
+		nonce++
+	}
+
+	return hashes, wallets, nil
+}
+
+func waitUntilMined(client *ethclient.Client, hashes []common.Hash) (bool, error) {
+	allMined := false
+	killSwitch := 0
+	for _, hash := range hashes {
+		for {
+			receipt, err := client.TransactionReceipt(context.Background(), hash)
+			if err != nil {
+				if errors.Is(err, ethereum.NotFound) {
+					time.Sleep(1 * time.Second)
+					killSwitch++
+					if killSwitch > 10 {
+						return false, errors.New("failed to mine all funding transactions")
+					}
+					continue
+				} else {
+					return false, err
+				}
+			}
+			if receipt.Status == types.ReceiptStatusSuccessful {
+				allMined = true
+				break
+			} else {
+				return false, errors.New("last mining transaction failed")
+			}
+		}
+	}
+	return allMined, nil
 }
